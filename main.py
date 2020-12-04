2b76469b8753efabd1a0214f205d82298fada602
Kevin Thomas <kevin@mytechnotalent.com>
2020-12-04 12:17:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from random import randint
 from microbit import display, Image, button_a, button_b
 from speech import say
 
+# Customize bot speaking speed
+SPEED = 95
+
 
 def play_game():
     """Play game 
@@ -27,39 +30,41 @@ def play_game():
     gc.collect()
     result = randint(0, 1)
     display.show(Image.MEH)
-    say('Let us play heads or tails.')
-    say('Press button A if you want to guess heads.')
-    say('Press button B if you want to guess tails.')
+    say('Let us play heads or tails.', speed=SPEED)
+    time.sleep(1)
+    say('Press button Ayy if you want to guess heads.', speed=SPEED)
+    time.sleep(1)
+    say('Press button B if you want to guess tails.', speed=SPEED)
     while True:
         if button_a.is_pressed():
             if result == 0:
                 display.show(Image.HAPPY)
-                say('Hooray!')
-                say('You guessed heads and you were right.')
-                say('You won!')
-                say('Press the RESET button to play again!')
+                say('Hooray!', speed=SPEED)
+                say('You guessed heads and you were right.', speed=SPEED)
+                say('You won!', speed=SPEED)
+                say('Press the RESET button to play again!', speed=SPEED)
                 break
             else:
                 display.show(Image.SAD)
-                say('Sorry!')
-                say('When I flipped my little coin it came up tails.')
-                say('Better luck next time!')
-                say('Press the RESET button to play again!')
+                say('Sorry!', speed=SPEED)
+                say('When I flipped my little coin it came up tails.', speed=SPEED)
+                say('Better luck next time!', speed=SPEED)
+                say('Press the RESET button to play again!', speed=SPEED)
                 break
         if button_b.is_pressed():
             if result == 1:
                 display.show(Image.HAPPY)
-                say('Hooray!')
-                say('You guessed tails and you were right.')
-                say('You won!')
-                say('Press the RESET button to play again!')
+                say('Hooray!', speed=SPEED)
+                say('You guessed tails and you were right.', speed=SPEED)
+                say('You won!', speed=SPEED)
+                say('Press the RESET button to play again!', speed=SPEED)
                 break
             else:
                 display.show(Image.SAD)
-                say('Sorry!')
-                say('When I flipped my little coin it came up heads.')
-                say('Better luck next time!')
-                say('Press the RESET button to play again!')
+                say('Sorry!', speed=SPEED)
+                say('When I flipped my little coin it came up heads.', speed=SPEED)
+                say('Better luck next time!', speed=SPEED)
+                say('Press the RESET button to play again!', speed=SPEED)
                 break
 
 
